ac4289dbd944229e814cbaa5d24009bd18949eb5
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-06-22 21:09:36 +0600

    1st push everything is working

---
+++ src/app/interface/book.interface.ts
@@ -0,0 +1,16 @@
+import { Document, Model } from "mongoose";
+
+export interface IBook {
+    title: string;
+    author:string;
+    genre:  "FICTION" | 'NON_FICTION' | 'SCIENCE' | 'HISTORY' | 'BIOGRAPHY' | 'FANTASY'
+    isbn:string;
+    description:string
+    copies:number
+    available:boolean
+    updateAvailableStatus(): Promise<void>;
+}
+
+export interface IBookModel extends Model<IBook> {
+    updateAvailableStatus(bookId: string): Promise<void>;
+}
